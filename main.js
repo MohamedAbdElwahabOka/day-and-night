550d7b8ce8519e1e18e24e6bef8d37cc087293c0
Mohamed Abd Elwahab Elazab Oka <93409090+MohamedAbdElwahabOka@users.noreply.github.com>
2022-07-18 18:30:10 +0200

    add  first files

---
+++ main.js
@@ -0,0 +1,16 @@
+// let stars = document.getElementById('stars');
+// let moon = document.getElementById('moon');
+// let mountains3 = document.getElementById('mountains3');
+// let mountains4 = document.getElementById('mountains4');
+// let river = document.getElementById('river');
+// let boat = document.getElementById('boat');
+// let novel = document.querySelector('.novel');
+// window.onscroll = function(){
+//     let value = scrollY;
+    
+    
+//     stars.style.left=value + 'px';
+
+   
+
+// }
